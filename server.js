84648ae9654da0413904c70513d901074c7a12f5
Hector Sanchez <hsanchez@opentext.com>
2023-12-11 03:16:53 -0700

    code commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const requireHTTPS = (request, response, next) => {
 	// eslint-disable-next-line
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ const HomePage = () => {
 		setError(null)
 		try {
 			const response = await axios.get(
-				`https://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_COCKTAIL_DB_API_KEY}/randomselection.php`
+				`https://www.thecocktaildb.com/api/json/v2/${process.env.COCKTAIL_DB_API_KEY}/randomselection.php`
 			)
 			const {drinks} = response.data
 			// @ts-expect-error
